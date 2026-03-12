6e9c61a08e3f12ba7c0fb75216f787bc91e0e209
Itamar Turner-Trauring <itamar@pythonspeed.com>
2026-03-12 16:08:14 -0400

    Tests pass.

--- kernel/src/table_features/mod.rs
+++ kernel/src/table_features/mod.rs
@@ -23,6 +23,7 @@ pub(crate) use column_mapping::{
     get_column_mapping_mode_from_properties,
 };
 pub(crate) use timestamp_ntz::validate_timestamp_ntz_feature_support;
+// TODO do equivalent for float16 -^
 mod column_mapping;
 mod timestamp_ntz;
 
